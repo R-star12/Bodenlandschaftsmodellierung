b3ab7a4519fe60e66256c24212ee010e6e7bef25
R-star12 <emil.unkrig@student.uni-tuebingen.de>
2026-01-10 14:54:56 +0100

    Regression-kriging

--- R-Code.R
+++ R-Code.R
@@ -48,7 +48,7 @@ str(cov_soil)
 
 
 
-###################### DESCRIPTIVE STATISTICS ###############################
+###################### DESCRIPTIVE STATISTICS ############################### #ANNALENA
 
 plot(cov_soil$CEC, cov_soil$NDVI)
 plot(cov_soil$CEC)
@@ -58,17 +58,106 @@ plot(cov_soil$CEC, cov_soil$Elevation)
 plot(cov_soil$Temperature)
 
 ## Korrelationsanalyse 
+
+library(ggcorrplot)
+
+# check the correlation between EC and covariates
+ggcorrplot(cor(cov_soil), hc.order = TRUE, type = "lower", lab = TRUE)
+
 ## Histogramme 
 
 
 ####################### MODEL ########################################
 
-#Lineare Regression 
-#Random Forest mit Regression Kriging 
+#Lineare Regression # ANNALENA
+
+#Random Forest mit Regression Kriging #EMIL
+
+# Erstellen eines neuen Datensatzes zum üben
+
+cov_soilA <- cov_soil
+
+
+# fit random forest model
+
+rf_fit <- randomForest(CEC ~ ., data = cov_soilA, ntree = 1000)
+
+# random forest prediction part 
+map_rf <- raster::predict(covariates_RS, rf_fit)
+
+# plot the RF map
+spplot(map_rf, main = "CEC map based on RF model")
+
+
+# append residuals
+cov_soilA$residuals <- cov_soilA$CEC - rf_fit$predicted
+names(cov_soilA)
+summary(cov_soilA)
+
+# histogram of the residuals 
+hist(cov_soilA$residuals, col = "lightblue")
+
+# convert cov_soil to spatial data
+cov_soilA$x <- soil_csv$x
+cov_soilA$y <- soil_csv$y
+coordinates(cov_soilA) <- ~ x + y
+proj4string(cov_soilA) <- CRS("+proj=utm +zone=39 +datum=WGS84 +units=m +no_defs")
+
+# compute experimental semivariogram of residuals
+library(gstat)
+
+gstat_res <- gstat(formula = residuals ~ 1, data = cov_soilA)
+vg_res    <- variogram(gstat_res)
+plot(vg_res, plot.nu = FALSE)
+
+# define initial semivariogram model
+vg_parameters_res <- vgm(nugget = 70, psill = 100, range = 1.5, model = "Pen")
+plot(vg_res, vg_parameters_res)
+
+# fit semivariogram model
+vg_model_res <- fit.variogram(vg_res, vg_parameters_res)
+plot(vg_res, vg_model_res)
+vg_model_res
+
+
+# export boundary as a grid (approx. 90 m) 
+res(covariates_RS) #Rastergröße etwa 1 km + 1 km
+r_template <- raster(study_area, res = 0.009)                 # template raster
+r_mask     <- rasterize(study_area, r_template, field = 1) # inside polygon = 1, outside = NA
+study_area_grid <- as(r_mask, "SpatialPixelsDataFrame")    # grid for kriging
+
+crs(cov_soilA) <- "EPSG:4326"
+
+# ordinary kriging of residuals
+res_krig <- krige(
+  formula   = residuals ~ 1,
+  locations = cov_soilA,
+  newdata   = study_area_grid,
+  model     = vg_model_res
+)
+
+# plot the residuals map
+spplot(res_krig, zcol = "var1.pred", main = "residuals predictions")
+
+
+
+# obtain regression kriging prediction
+res_krig_raster <- raster::resample(raster(res_krig), map_rf)
+
+RK_map <-     res_krig_raster +  map_rf ##evtl. umdrehen!?
+
+# plot the RK map
+spplot(RK_map, main = "CEC map based on RK model")
+
+
+
+
+
 
 ####################### MODELGÜTE #######################################
 
-#CV metrics; RMSE/MAE/R² 
-#Variable Importance 
+#CV metrics; RMSE/MAE/R² #CARLA
+
+#Variable Importance #CARLA
 
 
